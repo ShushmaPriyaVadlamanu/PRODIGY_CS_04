7b86307f583f5a1ab2cd0c712f5c888543d5f24a
ShushmaPriyaVadlamanu <149227381+ShushmaPriyaVadlamanu@users.noreply.github.com>
2024-06-29 10:17:37 +0530

    output.py

---
+++ output.py
@@ -0,0 +1,7 @@
+C:\Users\Sushma\PycharmProjects\PRODIGY_CS_04\.venv\Scripts\python.exe C:\Users\Sushma\PycharmProjects\PRODIGY_CS_04\keylogger.py 
+Starting keylogger...
+prodigy infotech cybersecurity internship task four
+simple key logger Esc key pressed. Exiting.
+Keylogger stopped.
+
+Process finished with exit code 0
